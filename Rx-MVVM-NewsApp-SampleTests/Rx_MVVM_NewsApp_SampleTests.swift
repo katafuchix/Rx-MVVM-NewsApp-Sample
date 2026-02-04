fa9e534c5928a4dc5a5d0003f56ccd2f2f85a58d
katafuchix <katafuchix@gmail.com>
2026-02-04 09:05:38 +0900

    pod deintegrate

---
+++ Rx-MVVM-NewsApp-SampleTests/Rx_MVVM_NewsApp_SampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  Rx_MVVM_NewsApp_SampleTests.swift
+//  Rx-MVVM-NewsApp-SampleTests
+//
+//  Created by cano on 2026/02/04.
+//
+
+import Testing
+
+struct Rx_MVVM_NewsApp_SampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
